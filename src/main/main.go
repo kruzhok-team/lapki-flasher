6dde55e111425628c117666ebcfd7b80126efd03
Roundabout1 <mrsivannap@gmail.com>
2023-07-06 15:29:56 +0800

    Add device detection

--- src/main/main.go
+++ src/main/main.go
@@ -16,15 +16,23 @@ import (
 	//"github.com/xela07ax/XelaGoDoc/encodingStdout"
 )
 
-type Board struct {
+type BoardType struct {
 	ProductID    string
-	VendorID     string
 	Name         string
 	Controller   string
 	Programmer   string
 	Bootloader   string
 	BootloaderID string
-	Port         int
+}
+
+func (board BoardType) hasBootloader() bool {
+	return board.BootloaderID != ""
+}
+
+type BoardToFlash struct {
+	Type     BoardType
+	VendorID string
+	Port     int
 }
 
 type UploadInfo struct {
@@ -70,6 +78,57 @@ func upload(data UploadInfo) {
 	transfer(data.Controller, data.Programmer, data.PortUpload, data.FilePath)
 }
 
+// todo: error handling
+func findDevice(ctx *gousb.Context, VID, PID string, port int) *gousb.Device {
+	devs, err := ctx.OpenDevices(func(desc *gousb.DeviceDesc) bool {
+		return VID == desc.Vendor.String() && PID == desc.Product.String() && (port == -1 || desc.Port == port)
+	})
+	fmt.Printf("devs: %v\n", devs)
+	if err != nil {
+		log.Fatalf("OpenDevices(): %v", err)
+	}
+	numberOfDevices := len(devs)
+	if numberOfDevices == 0 {
+		log.Fatalln("The device hasn't been found")
+	}
+	if numberOfDevices > 1 {
+		for _, d := range devs {
+			defer d.Close()
+		}
+		log.Fatalln("More than one device has been found")
+	}
+	return devs[0]
+}
+
+func flash(board BoardToFlash, file string) {
+	ctx := gousb.NewContext()
+	defer ctx.Close()
+	dev := findDevice(ctx, board.VendorID, board.Type.ProductID, board.Port)
+	defer dev.Close()
+	fmt.Println(dev.Desc.Configs)
+	for i, v := range dev.Desc.Configs {
+		fmt.Println(i, v)
+		for _, j := range v.Interfaces {
+			fmt.Println(j.String())
+		}
+	}
+	if board.Type.hasBootloader() {
+		err := dev.Reset()
+		if err != nil {
+			log.Fatalf("Coudln't reset the device: %v\n", err)
+		}
+		time.Sleep(time.Second)
+		ctxNew := gousb.NewContext()
+		bootloader := findDevice(ctxNew, board.VendorID, board.Type.BootloaderID, -1)
+		fmt.Println(bootloader)
+		bootloader.Close()
+		ctxNew.Close()
+	}
+	//flash := "flash:w:" + getAbolutePath(file) + ":a"
+	//fmt.Println(execString(getAbolutePath("avrdude/avrdude.exe"), "-p", controller, "-c", programmer, "-P", portUpload, "-U", flash))
+	//fmt.Println(execString("avrdude", "-p", board.Type.Controller, "-c", board.Type.Programmer, "-P", strconv.Itoa(board.Port), "-U", flash))
+}
+
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	var data UploadInfo
 	err := json.NewDecoder(r.Body).Decode(&data)
@@ -80,23 +139,36 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	upload(data)
 }
 
-func detect_boards() []Board {
+//go:embed device_list.txt
+var boards_list_str string
+
+//go:embed vendors.txt
+var vendors_list_str string
+
+func detect_boards() []BoardToFlash {
 	ctx := gousb.NewContext()
 	defer ctx.Close()
 	// list of supported vendors (should contain lower case only!)
-	vid := []string{"2341", "2a03"}
-	var boards []Board
+	vid := strings.Split(strings.ToLower(vendors_list_str), "\n")
+	var boards []BoardToFlash
+	groups := board_list()
 	_, err := ctx.OpenDevices(func(desc *gousb.DeviceDesc) bool {
-		//fmt.Println(desc.Product.String(), desc.Vendor.String())
 		// this function is called for every device present.
 		for _, v := range vid {
 			if strings.ToLower(desc.Vendor.String()) == v {
-				var board Board
-				board.ProductID = desc.Product.String()
-				board.Port = desc.Port
-				board.VendorID = desc.Vendor.String()
-				boards = append(boards, board)
-				break
+				//fmt.Println(v, desc.Product)
+				cur_group := groups[v]
+				var detectedBoard BoardToFlash
+				//fmt.Println(len(cur_group), v)
+				for _, board := range cur_group {
+					if board.ProductID == desc.Product.String() {
+						detectedBoard.VendorID = v
+						detectedBoard.Port = desc.Port
+						detectedBoard.Type = board
+						boards = append(boards, detectedBoard)
+						break
+					}
+				}
 			}
 		}
 		return false
@@ -107,16 +179,39 @@ func detect_boards() []Board {
 	return boards
 }
 
-//go:embed device_list.txt
-var str string
-
-func board_list() {
-	/*content, err := os.ReadFile("device_list.txt")
-	if err != nil {
-		log.Fatal(err)
+func board_list() map[string][]BoardType {
+	vendorGroups := make(map[string][]BoardType)
+	splitGroups := strings.Split(boards_list_str, ".\n")
+	//fmt.Println(splitGroups, len(splitGroups))
+	n := len(splitGroups) - 1
+	for i, v := range splitGroups {
+		if i == n {
+			break
+		}
+		//fmt.Println(v, len(v))
+		strs := strings.Split(v, "\n")
+		var cur_vendors []string
+		boards := make([]BoardType, len(strs)-1)
+		for j, s := range strs {
+			//fmt.Println(j, s)
+			if j == 0 {
+				cur_vendors = strings.Split(s, ",")
+			} else {
+				params := strings.Split(s, ";")
+				index := j - 1
+				boards[index].ProductID = params[0]
+				boards[index].Name = params[1]
+				boards[index].Controller = params[2]
+				boards[index].Programmer = params[3]
+				boards[index].Bootloader = params[4]
+				boards[index].BootloaderID = params[5]
+			}
+		}
+		for _, vendor := range cur_vendors {
+			vendorGroups[vendor] = boards
+		}
 	}
-	fmt.Println(string(content))*/
-	fmt.Println(str)
+	return vendorGroups
 }
 
 func setupRoutes() {
@@ -125,9 +220,14 @@ func setupRoutes() {
 }
 
 func main() {
-	/*board := detect_boards()
-	for i := range board {
-		fmt.Println(board[i].ProductID, board[i].Port)
-	}*/
-	board_list()
+	vendorGroups := board_list()
+	for i, v := range vendorGroups {
+		fmt.Printf("i: %s v: %v\n", i, v)
+	}
+	fmt.Println()
+	boards := detect_boards()
+	for _, board := range boards {
+		fmt.Printf("board: %v %t\n", board, board.Type.hasBootloader())
+	}
+	//flash(boards[0], "firmwares/blink.hex")
 }
