f97fd33b0653f1188f080da47c86eb1505bf7aca
Evolution <ivan.davydov@polyus-nt.ru>
2023-07-24 13:35:08 +0800

    Удаление неиспользуемой структуры

--- src/flasher.go
+++ src/flasher.go
@@ -4,11 +4,6 @@ import (
 	"fmt"
 )
 
-type UploadHeader struct {
-	Size    int
-	BoardID string
-}
-
 // прошивка
 func flash(board BoardToFlash, file string) {
 	flash := "flash:w:" + getAbolutePath(file) + ":a"
