b81aa99c6ee0e7050bb8c8b63d547a3a512a189d
Evolution <ivan.davydov@polyus-nt.ru>
2023-08-01 16:49:45 +0800

    Оптимизация поиска устройств

--- src/detection_windows.go
+++ src/detection_windows.go
@@ -13,9 +13,9 @@ import (
 )
 
 // возвращает пути к устройствам, согласно заданному шаблону, если они есть, иначе возвращает пустой массив (nil)
-func getInstanceId(path *string) []string {
+func getInstanceId(path string) []string {
 	start := time.Now()
-	cmd := fmt.Sprintf("Get-PnpDevice -PresentOnly -InstanceId '%s' | Select-Object -Property InstanceId", *path)
+	cmd := fmt.Sprintf("Get-PnpDevice -PresentOnly -InstanceId '%s' | Select-Object -Property InstanceId", path)
 	cmdExec := exec.Command("powershell", cmd)
 	cmdResult, err := cmdExec.CombinedOutput()
 	var possiblePathes []string
@@ -50,43 +50,50 @@ func getInstanceId(path *string) []string {
 
 // находит все подключённые платы, считает все найденные устройства за новые
 func detectBoards() map[string]*BoardToFlash {
+	startTime := time.Now()
 	vendors := vendorList()
 	boardTypes := boardList()
 	boards := make(map[string]*BoardToFlash)
-	for _, vendor := range vendors {
-		start := time.Now()
-		for _, boardType := range boardTypes[vendor] {
-			cmdPathPattern := fmt.Sprintf("USB\\VID_%s&PID_%s*", vendor, boardType.ProductID)
-			possiblePathes := getInstanceId(&cmdPathPattern)
-			// значит такое устройство не подключено
-			if possiblePathes == nil {
-				continue
-			}
-			// поиск портов, если удалось найти путь, то в список boards добавляется новое устройство
-			for _, path := range possiblePathes {
-				portName := findPortName(&path)
-				if portName == NOT_FOUND {
-					continue
+	presentUSBDevices := getInstanceId("USB*")
+	// нет usb-устройств
+	if presentUSBDevices == nil {
+		return nil
+	}
+	for _, line := range presentUSBDevices {
+		device := strings.TrimSpace(line)
+		deviceLen := len(device)
+		for _, vendor := range vendors {
+			for _, boardType := range boardTypes[vendor] {
+				pathPattern := fmt.Sprintf("USB\\VID_%s&PID_%s", vendor, boardType.ProductID)
+				pathLen := len(pathPattern)
+				// нашли подходящее устройство
+				if pathLen <= deviceLen && strings.ToLower(device[:pathLen]) == strings.ToLower(pathPattern) {
+					portName := findPortName(&device)
+					if portName == NOT_FOUND {
+						fmt.Println(device)
+						continue
+					}
+					boardType.VendorID = vendor
+					detectedBoard := NewBoardToFlash(boardType, portName)
+					serialIndex := strings.LastIndex(device, "\\")
+					possibleSerialID := device[serialIndex+1:]
+					if !strings.Contains(possibleSerialID, "&") {
+						detectedBoard.SerialID = device[serialIndex+1:]
+					}
+					boards[device] = detectedBoard
 				}
-				boardType.VendorID = vendor
-				detectedBoard := NewBoardToFlash(boardType, portName)
-				serialIndex := strings.LastIndex(path, "\\")
-				possibleSerialID := path[serialIndex+1:]
-				if !strings.Contains(possibleSerialID, "&") {
-					detectedBoard.SerialID = path[serialIndex+1:]
-				}
-				boards[path] = detectedBoard
 			}
 		}
-		end := time.Now()
-		fmt.Println(vendor, end.Sub(start))
 	}
+	endTime := time.Now()
+	fmt.Println("Detection time: ", endTime.Sub(startTime))
 	return boards
 }
 
 // возвращает имя порта, либо константу NOT_FOUND, если не удалось его не удалось найти
 func findPortName(instanceId *string) string {
 	keyPath := fmt.Sprintf("SYSTEM\\CurrentControlSet\\Enum\\%s\\Device Parameters", *instanceId)
+	fmt.Println(keyPath)
 	key, err := registry.OpenKey(registry.LOCAL_MACHINE, keyPath, registry.QUERY_VALUE)
 	if err != nil {
 		fmt.Println("Registry error:", err)
@@ -107,7 +114,7 @@ func findPortName(instanceId *string) string {
 
 // true - если порт изменился, иначе false
 func (board *BoardToFlash) updatePortName(ID string) bool {
-	instanceId := getInstanceId(&ID)
+	instanceId := getInstanceId(ID)
 	// такого устройства нет
 	if instanceId == nil {
 		board.PortName = NOT_FOUND
@@ -117,7 +124,7 @@ func (board *BoardToFlash) updatePortName(ID string) bool {
 		fmt.Printf("updatePortName: found more than one devices that are matched ID = %s\n", ID)
 		return false
 	}
-	portName := findPortName(&instanceId[0])
+	portName := findPortName(&ID)
 	if board.PortName != portName {
 		board.PortName = portName
 		return true
