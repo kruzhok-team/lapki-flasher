aef235dadac160482b62a6c86b7168985d36695f
Evolution <ivan.davydov@polyus-nt.ru>
2023-08-08 14:45:26 +0800

    Оптимизация поиска усройств

--- src/detection_windows.go
+++ src/detection_windows.go
@@ -5,46 +5,44 @@ package main
 
 import (
 	"fmt"
-	"os/exec"
 	"strings"
 	"time"
 
 	"golang.org/x/sys/windows/registry"
 )
 
-// возвращает пути к устройствам, согласно заданному шаблону, если они есть, иначе возвращает пустой массив (nil)
-func getInstanceId(path string) []string {
+// возвращает пути к устройствам, согласно заданному шаблону, если они есть, иначе возвращает nil
+func getInstanceId(substring string) []string {
 	start := time.Now()
-	cmd := fmt.Sprintf("Get-PnpDevice -PresentOnly -InstanceId '%s' | Select-Object -Property InstanceId", path)
-	cmdExec := exec.Command("powershell", cmd)
-	cmdResult, err := cmdExec.CombinedOutput()
-	var possiblePathes []string
-	// значит такое устройство не подключено
+	keyPath := "SYSTEM\\CurrentControlSet\\Services\\usbser\\Enum"
+	key, err := registry.OpenKey(registry.LOCAL_MACHINE, keyPath, registry.QUERY_VALUE)
+	defer key.Close()
 	if err != nil {
-		end := time.Now()
-		fmt.Println("get instance", end.Sub(start))
-		return possiblePathes
+		fmt.Println("Can't find devices", err)
+		return nil
 	}
-
-	// получение возможных путей к устройствам
-
-	curStr := ""
-	for _, v := range cmdResult {
-		if v == '\n' {
-			if len(curStr) > 0 && curStr[0] == 'U' {
-				possiblePathes = append(possiblePathes, curStr)
+	registryValues, err := key.ReadValueNames(0)
+	if err != nil {
+		fmt.Println("Error on getting registry value names", err.Error())
+		return nil
+	}
+	var possiblePathes []string
+	substring = strings.ToLower(substring)
+	for _, valueName := range registryValues {
+		value, _, err := key.GetStringValue(valueName)
+		if err != nil {
+			if err == registry.ErrUnexpectedType {
+				continue
 			}
-			curStr = ""
+			fmt.Println("Error on getting registry values", err.Error())
 			continue
 		}
-		// Carriage Return code = 13
-		if v == 13 {
-			continue
+		if strings.Contains(strings.ToLower(value), substring) {
+			possiblePathes = append(possiblePathes, value)
 		}
-		curStr += string(v)
+		fmt.Println(value)
 	}
-	end := time.Now()
-	fmt.Println("get instance", end.Sub(start))
+	fmt.Println("get instance", time.Now().Sub(start))
 	return possiblePathes
 }
 
@@ -54,7 +52,7 @@ func detectBoards() map[string]*BoardToFlash {
 	vendors := vendorList()
 	boardTypes := boardList()
 	boards := make(map[string]*BoardToFlash)
-	presentUSBDevices := getInstanceId("USB*")
+	presentUSBDevices := getInstanceId("usb")
 	// нет usb-устройств
 	if presentUSBDevices == nil {
 		return nil
@@ -67,6 +65,7 @@ func detectBoards() map[string]*BoardToFlash {
 				pathPattern := fmt.Sprintf("USB\\VID_%s&PID_%s", vendor, boardType.ProductID)
 				pathLen := len(pathPattern)
 				// нашли подходящее устройство
+				fmt.Println(strings.ToLower(device[:pathLen]), strings.ToLower(pathPattern))
 				if pathLen <= deviceLen && strings.ToLower(device[:pathLen]) == strings.ToLower(pathPattern) {
 					portName := findPortName(&device)
 					if portName == NOT_FOUND {
@@ -95,6 +94,7 @@ func findPortName(instanceId *string) string {
 	keyPath := fmt.Sprintf("SYSTEM\\CurrentControlSet\\Enum\\%s\\Device Parameters", *instanceId)
 	fmt.Println(keyPath)
 	key, err := registry.OpenKey(registry.LOCAL_MACHINE, keyPath, registry.QUERY_VALUE)
+	defer key.Close()
 	if err != nil {
 		fmt.Println("Registry error:", err)
 		return NOT_FOUND
