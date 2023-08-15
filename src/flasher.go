49600316779b229d1ab87e209d8c90432d0e4d12
Ivan Davydov <ivan.davydov@polyus-nt.ru>
2023-08-15 13:05:05 +0800

    К сообщению от avrdude прикрепляется сообщение об ошибке (если есть)

--- src/flasher.go
+++ src/flasher.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"time"
 )
 
 const AVRDUDE = "avrdude"
@@ -10,10 +11,19 @@ const AVRDUDE = "avrdude"
 // прошивка,
 // ожидается, что плата заблокирована (board.IsFlashBlocked() == true)
 func flash(board *BoardToFlash, filePath string) (avrdudeMessage string, err error) {
+	time.Sleep(10 * time.Second)
 	flash := "flash:w:" + getAbolutePath(filePath) + ":a"
 	fmt.Println(AVRDUDE, "-p", board.Type.Controller, "-c", board.Type.Programmer, "-P", board.PortName, "-U", flash)
 	cmd := exec.Command(AVRDUDE, "-p", board.Type.Controller, "-c", board.Type.Programmer, "-P", board.PortName, "-U", flash)
 	stdout, err := cmd.CombinedOutput()
-	avrdudeMessage = string(stdout)
+	outputString := string(stdout)
+	if err != nil {
+		avrdudeMessage = err.Error()
+		if outputString != "" {
+			avrdudeMessage += "\n" + outputString
+		}
+	} else {
+		avrdudeMessage = outputString
+	}
 	return
 }
