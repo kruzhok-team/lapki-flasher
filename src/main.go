62283490e93dac554c18399ad83cceb2e1303df1
Ivan Davydov <ivan.davydov@polyus-nt.ru>
2023-08-17 12:19:32 +0800

    Удаление лишних кусков кода

--- src/main.go
+++ src/main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	_ "embed"
-	"fmt"
 	"log"
 	"net/http"
-	"time"
 )
 
 //go:embed index.html
@@ -15,30 +13,13 @@ const webPort = ":8080"
 
 var detector *Detector
 
-func setupRoutes() {
-	//http.HandleFunc("/", showJS)
-	//http.HandleFunc("/upload", uploadHandler)
-	log.Fatal(http.ListenAndServe(webPort, nil))
-}
-
 func showJS(w http.ResponseWriter, r *http.Request) {
 	w.Write(staticPage)
 }
 
-func test() {
-	/*start := time.Now()
-	detector.Update()
-	end := time.Now()
-	fmt.Println(end.Sub(start))*/
-	start := time.Now()
-	detectBoards()
-	end := time.Now()
-	fmt.Println(end.Sub(start))
-}
-
 func main() {
 	detector = NewDetector()
-	//test()
+
 	manager := NewWebSocketManager()
 
 	http.HandleFunc("/", showJS)
