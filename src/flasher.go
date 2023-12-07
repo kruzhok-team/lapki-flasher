b9efc3ed762a2ec29a0ca3ed06bccd75aa5194c3
Roundabout1 <mrsivannap@gmail.com>
2023-12-07 15:19:53 +0800

    Добавление новых аргументов для прошивки
    avrdudePath и configPath

--- src/flasher.go
+++ src/flasher.go
@@ -5,14 +5,16 @@ import (
 	"time"
 )
 
-const AVRDUDE = "avrdude"
-
 // прошивка,
 // ожидается, что плата заблокирована (board.IsFlashBlocked() == true)
-func flash(board *BoardToFlash, filePath string) (avrdudeMessage string, err error) {
-	flash := "flash:w:" + getAbolutePath(filePath) + ":a"
-	printLog(AVRDUDE, "-p", board.Type.Controller, "-c", board.Type.Programmer, "-P", board.PortName, "-U", flash)
-	cmd := exec.Command(AVRDUDE, "-p", board.Type.Controller, "-c", board.Type.Programmer, "-P", board.PortName, "-U", flash)
+func flash(board *BoardToFlash, hexFilePath string) (avrdudeMessage string, err error) {
+	flash := "flash:w:" + getAbolutePath(hexFilePath) + ":a"
+	args := []string{"-p", board.Type.Controller, "-c", board.Type.Programmer, "-P", board.PortName, "-U", flash}
+	if configPath == "" {
+		args = append(args, "-C", configPath)
+	}
+	printLog(avrdudePath, args)
+	cmd := exec.Command(avrdudePath, args...)
 	stdout, err := cmd.CombinedOutput()
 	outputString := string(stdout)
 	if err != nil {
