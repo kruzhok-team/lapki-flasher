00ed463aa433d5b97bdb5907de079a2017e4f106
Roundabout1 <mrsivannap@gmail.com>
2023-12-07 14:17:32 +0800

    Добавлена опция -D, без которой нельзя прошить Mega

--- src/flasher.go
+++ src/flasher.go
@@ -11,8 +11,9 @@ const AVRDUDE = "avrdude"
 // ожидается, что плата заблокирована (board.IsFlashBlocked() == true)
 func flash(board *BoardToFlash, filePath string) (avrdudeMessage string, err error) {
 	flash := "flash:w:" + getAbolutePath(filePath) + ":a"
-	printLog(AVRDUDE, "-p", board.Type.Controller, "-c", board.Type.Programmer, "-P", board.PortName, "-U", flash)
-	cmd := exec.Command(AVRDUDE, "-p", board.Type.Controller, "-c", board.Type.Programmer, "-P", board.PortName, "-U", flash)
+	printLog(AVRDUDE, "-D", "-p", board.Type.Controller, "-c", board.Type.Programmer, "-P", board.PortName, "-U", flash)
+	// без опции "-D" не может прошить Arduino Mega
+	cmd := exec.Command(AVRDUDE, "-D", "-p", board.Type.Controller, "-c", board.Type.Programmer, "-P", board.PortName, "-U", flash)
 	stdout, err := cmd.CombinedOutput()
 	outputString := string(stdout)
 	if err != nil {
