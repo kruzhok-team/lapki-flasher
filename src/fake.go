4c6d12a62164f18882fdc57816f71cb780aa1f14
Roundabout1 <mrsivannap@gmail.com>
2024-09-24 12:01:36 +0800

    Update fake.go

--- src/fake.go
+++ src/fake.go
@@ -1,8 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 type FakeBoard struct {
 	controller string
 	programmer string
 	serialID   string
 	portName   string
+}
+
+// подключено ли устройство
+func (board *FakeBoard) IsConnected() bool {
+	return true
+}
+
+func (board *FakeBoard) GetSerialPort() string {
+	return board.portName
+}
+
+func (board *FakeBoard) Flash(filePath string) (string, error) {
+	time.Sleep(3 * time.Second)
+	printLog(fmt.Sprintf("Fake uploading of file %s in board %v is completed", filePath, board))
+	fakeMessage := "Fake flashing is completed"
+	return fakeMessage, nil
+}
+
+func (board *FakeBoard) Update() bool {
+	return false
 }
