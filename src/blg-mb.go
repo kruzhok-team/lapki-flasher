b281c1df01878afa59dd0be6055554b6e47e2adb
Roundabout1 <mrsivannap@gmail.com>
2025-08-04 16:18:48 +0800

    функция получения версии КиберМишки

--- src/blg-mb.go
+++ src/blg-mb.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"errors"
+	"fmt"
 	"os/exec"
+	"strings"
 )
 
 type BlgMb struct {
@@ -59,4 +63,32 @@ func (board *BlgMb) Update() bool {
 func (board *BlgMb) GetMetaData() (any, error) {
 	stdout, stderr := board.CyberBearLoader("identify")
 	return string(stdout), stderr
+}
+
+/*
+Функция получения версии КиберМишки
+*/
+func (board *BlgMb) GetVersion() (string, error) {
+	value, err := board.GetMetaData()
+	if err != nil {
+		return "", err
+	}
+	meta, ok := value.(string)
+	if !ok {
+		return "", errors.New("ошибка преобразования данных при попытке получить версию КиберМишки")
+	}
+
+	scanner := bufio.NewScanner(strings.NewReader(meta))
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if strings.HasPrefix(line, "art:") {
+			parts := strings.SplitN(line, ":", 2)
+			if len(parts) == 2 {
+				return strings.TrimSpace(parts[1]), nil
+			}
+		}
+	}
+
+	return "", fmt.Errorf("art value not found")
 }
